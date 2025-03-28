a1ab4a3773d6504a8c6248f25718e61eb346bf54
1000hyehyang <ducogus12@gmail.com>
2025-03-29 00:02:25 +0900

    Initial commit

---
+++ src/test/java/com/imchankyu/chankyu_archive/ChankyuArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imchankyu.chankyu_archive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChankyuArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
