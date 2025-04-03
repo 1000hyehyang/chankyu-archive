dd152733dac2cf2053ff759cff301b2675b6dafb
1000hyehyang <ducogus12@gmail.com>
2025-04-04 01:14:58 +0900

    feat : vercel 배포 url config에...

--- src/main/java/com/imchankyu/config/WebConfig.java
+++ src/main/java/com/imchankyu/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://your-vercel-domain.vercel.app")
+                .allowedOrigins("http://localhost:5173", "https://chankyu-archive.vercel.app")
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
